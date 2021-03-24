84b4f7fc65800a9b2816ae82740f0892e786c082
monokuro49 <07akhmadnur@gmail.com>
2021-03-24 21:12:38 +0700

    Tugas Minggu 3 Laravel

--- Minggu3_2/Latihan1/routes/web.php
+++ Minggu3_2/Latihan1/routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('user', "ManagementUserController@index");
+Route::resource("user", "ManagementUserController");
