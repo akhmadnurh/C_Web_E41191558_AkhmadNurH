dc3835cb57ffd675869f82a2c969b0f4ff725abc
monokuro49 <07akhmadnur@gmail.com>
2021-03-31 13:50:55 +0700

    Tugas Minggu3 routes

--- Minggu3/Latihan1/routes/web.php
+++ Minggu3/Latihan1/routes/web.php
@@ -13,11 +13,9 @@ use App\Http\Controllers\ManagementUserController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
 // Route::get('user', "ManagementUserController@index");
-// Route::get("/user", [ManagementUserController::class, "index"]);
+Route::get("/user", [ManagementUserController::class, "index"]);
 Route::get("/home", function(){
     return view("home", ["nama" => "Akhmad Nur Hidayatulloh", "pelajaran" => ["Algoritma & Pemrograman", "Kalkulus", "Pemrograman Web"]]);
 });
