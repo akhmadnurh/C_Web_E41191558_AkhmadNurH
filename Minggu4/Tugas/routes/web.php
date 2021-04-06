1415ab495e51d616e3d7b93fabb46a21a502afb7
monokuro49 <07akhmadnur@gmail.com>
2021-04-06 20:04:34 +0700

    nothing changed

--- Minggu4/Tugas/routes/web.php
+++ Minggu4/Tugas/routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
+namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\frontend\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get("home", function(){
-    return view("frontend.home");
-});
+// Route::get("home", function(){
+//     return view("frontend.home");
+// });
+Route::get('home', [HomeController::class, 'index']);
